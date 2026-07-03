4862204d0b9344dee666098c3140c6a6b7130362
ShoichiF19 <shawnmarcoreyes@gmail.com>
2026-07-04 00:49:30 +0800

    Nerd

--- school.php
+++ school.php
@@ -1,4 +1,15 @@
 <!-- Page where users may edit their previous reviews -->
 <!-- Accessed through home.php -->
- 
-<?php include 'navbar.php'; ?>
+
+<?php include 'navbar.php'; 
+
+$uni_id = $_GET['uni_id'];
+
+$sql = "SELECT * FROM teachers WHERE uni_id = '$uni_id'";
+$result = mysqli_query($conn, $sql);
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo $row['teacher_fname'] . " " . $row['teacher_lname'] . "<br>";
+}
+
+?>
