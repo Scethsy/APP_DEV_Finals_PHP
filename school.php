ebafbadb96e1b67eef8dd5a3d81d849caa64f8e3
ShoichiF19 <shawnmarcoreyes@gmail.com>
2026-07-04 00:55:26 +0800

    Nerd

--- school.php
+++ school.php
@@ -9,7 +9,10 @@ $sql = "SELECT * FROM teachers WHERE uni_id = '$uni_id'";
 $result = mysqli_query($conn, $sql);
 
 while ($row = mysqli_fetch_assoc($result)) {
-    echo $row['teacher_fname'] . " " . $row['teacher_lname'] . "<br>";
+    echo "<div>";
+    echo "<h3>" . htmlspecialchars($row['teacher_fname'] . " " . $row['teacher_lname']) . "</h3>";
+    echo "<a href='teacher_profile.php?teacher_id=" . $row['teacher_id'] . "'>View Profile</a>";
+    echo "</div>";
 }
 
 ?>
