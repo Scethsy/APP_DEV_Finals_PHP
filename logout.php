f4b14e216620e1fe30f740b9c8193d224c73fc7c
ShoichiF19 <shawnmarcoreyes@gmail.com>
2026-07-03 20:18:45 +0800

    Add logout.php

--- logout.php
+++ logout.php
@@ -1,2 +1,11 @@
 <!-- Page when users logout -->
 <!-- Accessed through homepage.php -->
+
+<?php 
+
+    session_start();
+    session_destroy();
+
+?>
+
+You have successfully logged out! Thank you for using LectSure! Please click here to <a href="login.php"> login </a>.
