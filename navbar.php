d3959ab1e094fd5bbc52e0f3a6ebd1ae041d1835
ShoichiF19 <shawnmarcoreyes@gmail.com>
2026-07-04 00:36:05 +0800

    Nerd

--- navbar.php
+++ navbar.php
@@ -1,6 +1,34 @@
-    <nav>
-    LectSure
-    <a href="homepage.php">Home</a>
-    <a href="school.php">Universities</a>
+<?php
+include 'connection.php';
+$universities = mysqli_query($conn, "SELECT uni_id, uni_name FROM universities");
+?>
+
+<nav>
     <a href="profile.php">Profile</a>
-    </nav>
+<?php
+include 'connection.php';
+$universities = mysqli_query($conn, "SELECT uni_id, uni_name FROM universities");
+?>
+
+<nav>
+    <a href="profile.php">Profile</a>
+
+    <form method="get" action="school.php" style="display:inline;">
+        <select name="uni_id" onchange="this.form.submit()">
+            <option value="">University</option>
+
+            <?php while ($row = mysqli_fetch_assoc($universities)) { ?>
+                <option value="<?php echo $row['uni_id']; ?>">
+                    <?php echo htmlspecialchars($row['uni_name']); ?>
+                </option>
+            <?php } ?>
+        </select>
+    </form>
+
+    <a href="logout.php">Logout</a>
+</nav>
+
+    </a>
+
+    <a href="logout.php">Logout</a>
+</nav>
