f81b37a7d7bd678ddd7696995f900d82465d117e
nathan <dannathan.valeriano@gmail.com>
2026-07-03 23:40:40 +0800

    progress on edit profile and review

--- edit_profile.php
+++ edit_profile.php
@@ -14,11 +14,60 @@ $user = mysqli_fetch_assoc($result);
 ?>
 <form method="post" action="profile.php">
     <!-- Name -->
-    <input type = "text" name = "fname" value = "<?php echo htmlspecialchars($user['fname']); ?>" required>
-    <input type = "text" name = "lname" value = "<?php echo htmlspecialchars($user['lname']); ?>" required>
+    <input type = "text" name = "fname" value = "<?php echo htmlspecialchars($user['fname']); ?>" required><br>
+    <input type = "text" name = "lname" value = "<?php echo htmlspecialchars($user['lname']); ?>" required><br>
     <!-- Email -->
-    <input type = "email" name = "email" value = "<?php echo htmlspecialchars($user['email']); ?>" required>
+    <input type = "email" name = "email" value = "<?php echo htmlspecialchars($user['email']); ?>" required><br>
+    <!-- Password -->
+    Enter New Password (Dont input if you don't want to change it)
+    <input type="password" name="pass" placeholder="Optional:"><br>
+    <input type="password" name="pass2" placeholder="Confirm Password"><br>
     <!-- University -->
+    <select name="uni_id" required>
+        <option value="">Select University</option>
 
-    <input type="submit" value="Sign Up" name="signup">
+        <?php
+        $universities = mysqli_query($conn, "SELECT uni_id, uni_name FROM universities");
+
+        while ($row = mysqli_fetch_assoc($universities)) {
+            $selected = ($row['uni_id'] == $user['uni_id']) ? "selected" : "";
+
+            echo "<option value='" . $row['uni_id'] . "' $selected>";
+            echo htmlspecialchars($row['uni_name']);
+            echo "</option>";
+        }
+        ?>
+    </select><br><br>
+
+    <input type="submit" value="Edit Profile" name="signup">
 </form>
+
+<?php
+if (isset($_POST['signup'])) {
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    $pass2 = $_POST['pass2'];
+    $uni_id= $_POST['uni_id'];
+
+    if ($pass == $pass2){
+
+        $hashedPassword = password_hash($pass, PASSWORD_DEFAULT);
+
+        $sql = "INSERT INTO users(fname, lname, email, password, uni_id)
+                VALUES('$fname', '$lname', '$email', '$pass', '$uni_id')";
+
+        $result = mysqli_query($conn, $sql);
+        
+        if ($result){
+            echo "Profile Updated!";
+        } else {
+            echo "Error while signing up.";
+        }
+    } else{
+        echo "Password do not match!";
+    }
+
+}
+?>
--- review.php
+++ review.php
@@ -4,13 +4,30 @@
 include 'connection.php';
 
     $universities = mysqli_query($conn, "SELECT uni_id, uni_name FROM universities");
+    $teachers = mysqli_query($conn, "SELECT teacher_id, teacher_fname, teacher_lname, uni_id FROM teachers")
 
 ?>
 
 <form method = "post" action = "teacher.php">
     <!-- Name -->
-    <input type="text" name="fname" placeholder="Given name" required><br>
-    <input type="text" name="lname" placeholder="Surname" required><br>
+    <select name="teacher_id">
+        <option value="">-- Select Existing Teachers --</option>
+
+        <?php
+        while ($row = mysqli_fetch_assoc($teachers)) {
+            echo "<option value='{$row['teacher_id']}'>";
+            echo htmlspecialchars($row['teacher_fname'] . " " . $row['teacher_lname']);
+            echo "</option>";
+        }
+        ?>
+
+    </select><br><br>
+
+    Add New Teacher
+    <input type="text" name="teacher_fname" placeholder="Given name"><br>
+    <input type="text" name="teacher_lname" placeholder="Surname"><br>
+
+    <!-- Course Code -->
     <input type="text" name="course_code" placeholder="Course Code" required><br>
 
     <!-- University Dropdown -->
@@ -84,25 +101,35 @@ include 'connection.php';
 
 <?php
 if (isset($_POST['signup'])) {
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
+    $teacher_id = $_POST['teacher_id'];
+    $teacher_fname = $_POST['teacher_fname'];
+    $teacher_lname = $_POST['teacher_lname'];
     $course_code = $_POST['course_code'];
     $approach_rating = $_POST['approach_rating'];
     $knowledge_rating = $_POST['knowledge_rating'];
     $strict_level = $_POST['strict_level'];
     $time_man_rating = $_POST['time_man_rating'];
     $comments = $_POST['comments'];
-    //fname and lname still issues to solve
-    $sql = "INSERT INTO users(fname, lname, course_code, approach_rating, knowledge_rating, strict_level, time_man_rating, )
-            VALUES('$fname', '$lname', '$course_code', '$approach_rating', '$knowledge_rating', '$strict_level', '$time_man_rating')";
+    if (trim($teacher_fname === "") || trim($teacher_lname === "")) { //Existing Teacher
+        $sql = "INSERT INTO reviews()
+                VALUES('',)";
+    } else if ($teacher_id == "") { //New Teacher
+        $sql = "INSERT INTO teachers(teacher_fname, teachers_lname, course_code, approach_rating, knowledge_rating, strict_level, time_man_rating, )
+                VALUES('$teacher_fname', '$teacher_lname', '$course_code', '$approach_rating', '$knowledge_rating', '$strict_level', '$time_man_rating')";
+    } else if (!(trim($teacher_fname === "") || trim($teacher_lname === "")) && !($teacher_id == "")) { //If User Did Both
+
+    } else if ((trim($teacher_fname === "") || trim($teacher_lname === "")) && $teacher_id == "") { //If User Did Neither
+
+    } 
+
     
-    $result = mysqli_query($conn, $sql);
+    // $result = mysqli_query($conn, $sql);
     
-    if ($result){
-        echo "Review for Professor $fname $lname has been added successfully.";
-    } else {
-        echo "Error while submitting review. Please try again.";
-    }
+    // if ($result){
+    //     echo "Review for Professor $fname $lname has been added successfully.";
+    // } else {
+    //     echo "Error while submitting review. Please try again.";
+    // }
 
 }
 ?>
