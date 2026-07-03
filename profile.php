7bc5d15717e97cb9bee28dd328938efb9d6590d6
ShoichiF19 <shawnmarcoreyes@gmail.com>
2026-07-03 20:18:02 +0800

    Add profile.php

--- profile.php
+++ profile.php
@@ -1,2 +1,14 @@
 <!-- Page where users may view their profile -->
 <!-- Accessed through home.php -->
+
+<?php 
+    session_start();
+
+    if(!isset($_SESSION['user'])){
+        header("location: login.php");
+    }
+?>
+
+<h1> Welcome <?php echo $_SESSION['user']; ?>! </h1>
+
+<a href="logout.php">logout</a>
