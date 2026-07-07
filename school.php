75cda28a9a5747c827fc85c74016358e3673b1b2
ShoichiF19 <shawnmarcoreyes@gmail.com>
2026-07-07 23:06:03 +0800

    Add signup.php

--- school.php
+++ school.php
@@ -21,7 +21,7 @@ $result = mysqli_query($conn, $sql);
 while ($row = mysqli_fetch_assoc($result)) {
     echo "<div>";
     echo "<h3>" . htmlspecialchars($row['teacher_fname'] . " " . $row['teacher_lname']) . "</h3>";
-    echo "<a href='teacher_profile.php?teacher_id=" . $row['teacher_id'] . "'>View Profile</a>";
+    echo "<a href='teacher.php?teacher_id=" . $row['teacher_id'] . "'>View Profile</a>";
     echo "<br>---------------------------------------------------------------";
     echo "</div>";
 }
